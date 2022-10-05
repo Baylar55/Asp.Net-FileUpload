d4bea6cc2eaaa8b4827f69518c20e7ad53c84c1c
Beyler55 <baylarss@code.edu.az>
2022-10-05 17:41:37 +0400

    front-to-back

---
+++ PurpleBuzz-Backend/Models/RecentWorkComponent.cs
@@ -0,0 +1,9 @@
+﻿namespace PurpleBuzz_Backend.Models
+{
+    public class RecentWorkComponent
+    {
+        public string Title { get; set; } 
+        public string Text { get; set; } 
+        public string FilePath { get; set; } 
+    }
+}
